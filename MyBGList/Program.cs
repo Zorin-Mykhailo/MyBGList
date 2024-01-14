97b0f4211a9e05eea7f63be394dbaedb3f9dac77
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2024-01-14 05:20:25 +0200

    WIP

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,15 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]!);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    opt.AddPolicy(name: "AnyOrigin", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
