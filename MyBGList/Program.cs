c27b139c6732adb28d3b4bc58e9ce9be0f45af66
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2024-01-14 06:01:20 +0200

    WIP

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -18,8 +20,6 @@ builder.Services.AddCors(opt =>
     });
 });
 
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -31,18 +31,18 @@ if(app.Configuration.GetValue<bool>("UseSwagger"))
     app.UseSwaggerUI();
 }
 
-if(app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
-    app.UseDeveloperExceptionPage();
-else
-    app.UseExceptionHandler("/error");
+if(app.Configuration.GetValue<bool>("UseDeveloperExceptionPage")) app.UseDeveloperExceptionPage();
+else app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("Test"); });
+app.MapGet("/error", [EnableCors("AnyOrigin")]() => Results.Problem());
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => { throw new Exception("Test"); }).RequireCors("AnyOrigin");
 
-app.MapControllers();
+app.MapControllers().RequireCors("AnyOrigin");
 
 app.Run();
