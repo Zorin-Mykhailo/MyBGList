3c1d2b0d31a35881ca7bf2d0479bbd208115c1f3
ZorinM <zorin.mikhail.84284@gmail.com>
2024-01-16 16:53:53 +0200

    WIP

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -13,14 +13,15 @@ public class BoardGamesController : ControllerBase
     private readonly AppDbContext _context;
     private readonly ILogger<BoardGamesController> _logger;
 
+
     public BoardGamesController(AppDbContext appDbContext, ILogger<BoardGamesController> logger)
     {
         _context = appDbContext;
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetBoardGames")]
-    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
+
+    [HttpGet(Name = "GetBoardGames"), ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
     public async Task<RestDTO<BoardGame[]>> GetAsync(int pageIndex = 0, int pageSize = 10, string? sortColumn = "Name", string? sortOrder = "ASC", string? nameFilter = null)
     {
         var query = _context.BoardGames.AsQueryable();
@@ -45,6 +46,29 @@ public class BoardGamesController : ControllerBase
             PageSize = pageSize,
             RecordCount = filteredRecordsCount ?? await _context.BoardGames.CountAsync(),
             Links = [new(Url.Action(null, "BoardGame", new { pageIndex, pageSize }, Request.Scheme)!, "self", "GET")]
+        };
+    }
+
+
+    [HttpPost(Name = "UpdateBoardGame"), ResponseCache(NoStore = true)]
+    public async Task<RestDTO<BoardGame?>> PostAsync(BoardGameDTO model)
+    {
+        var boardGame = await _context.BoardGames.Where(e => e.Id == model.Id).FirstOrDefaultAsync();
+        if(boardGame != null)
+        {
+            if(!string.IsNullOrEmpty(model.Name))
+                boardGame.Name = model.Name;
+            if(model.Year.HasValue && model.Year.Value > 0)
+                boardGame.Year = model.Year.Value;
+            boardGame.LastModifiedDate = DateTime.Now;
+            _context.BoardGames.Update(boardGame);
+            await _context.SaveChangesAsync();
+        }
+
+        return new RestDTO<BoardGame?>()
+        {
+            Data = boardGame,
+            Links = [new(Url.Action(null, "BoardGames", model, Request.Scheme)!, "self", "POST")]
         };
     }
 }
