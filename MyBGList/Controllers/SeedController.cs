e68e5328948ef11b36df94af750feefd46469c1a
ZorinM <zorin.mikhail.84284@gmail.com>
2024-01-16 08:58:29 +0200

    WIP

--- MyBGList/Controllers/SeedController.cs
+++ MyBGList/Controllers/SeedController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyBGList.Models;
+using MyBGList.Models.Csv;
+using System.Globalization;
 
 namespace MyBGList.Controllers;
 
@@ -19,5 +24,109 @@ public class SeedController : ControllerBase
         _context = context;
         _env = env;
         _logger = logger;
+    }
+
+    [HttpPut(Name = "Seed")]
+    [ResponseCache(NoStore = true)]
+    public async Task<JsonResult> PutAsync()
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            Delimiter = ";",
+        };
+        using StreamReader reader = new(Path.Combine(_env.ContentRootPath, "Data/bgg_dataset.csv"));
+        using CsvReader csv = new CsvReader(reader, config);
+        Dictionary<int, BoardGame> existingBoardGames = await _context.BoardGames.ToDictionaryAsync(k => k.Id);
+        Dictionary<string, Domain> existingDomains = await _context.Domains.ToDictionaryAsync(k => k.Name);
+        Dictionary<string, Mechanic> existingMechanics = await _context.Mechanics.ToDictionaryAsync(k => k.Name);
+        DateTime now = DateTime.Now;
+
+        
+        IEnumerable<BggRecord> records = csv.GetRecords<BggRecord>();
+        int skippedRows = 0;
+        foreach (var record in records)
+        {
+            if (!record.ID.HasValue || string.IsNullOrEmpty(record.Name) || existingBoardGames.ContainsKey(record.ID.Value))
+            {
+                skippedRows++;
+                continue;
+            }
+            BoardGame boardGame = new()
+            {
+                Id = record.ID.Value,
+                Name = record.Name,
+                BGGRank = record.BGGRank ?? 0,
+                ComplexityAverage = record.ComplexityAverage ?? 0,
+                MaxPlayers = record.MaxPlayers ?? 0,
+                MinAge = record.MinAge ?? 0,
+                MinPlayers = record.MinPlayers ?? 0,
+                OwnedUsers = record.OwnedUsers ?? 0,
+                PlayTime = record.PlayTime ?? 0,
+                RatingAverage = record.RatingAverage ?? 0,
+                UsersRelated = record.UsersRated ?? 0,
+                Year = record.YearPublished ?? 0,
+                CreatedDate = now,
+                LastModifiedDate = now,
+            };
+            _context.BoardGames.Add(boardGame);
+
+            if(!string.IsNullOrEmpty(record.Domains))
+                foreach(var domainName in record.Domains.Split(",", StringSplitOptions.TrimEntries).Distinct(StringComparer.InvariantCultureIgnoreCase))
+                {
+                    Domain? domain = existingDomains.GetValueOrDefault(domainName);
+                    if(domain == null)
+                    {
+                        domain = new()
+                        {
+                            Name = domainName,
+                            CreatedDate = now,
+                            LastModifiedDate = now
+                        };
+                        _context.Domains.Add(domain);
+                        existingDomains.Add(domainName, domain);
+                    }
+                    _context.BoardGames_Domains.Add(new ()
+                    {
+                        BoardGame = boardGame,
+                        Domain = domain,
+                        CreatedDate = now
+                    });
+                }
+
+            if(!string.IsNullOrEmpty(record.Mechanics))
+                foreach(var mechanicName in record.Mechanics.Split(",", StringSplitOptions.TrimEntries).Distinct(StringComparer.InvariantCultureIgnoreCase))
+                {
+                    var mechanic = existingMechanics.GetValueOrDefault(mechanicName);
+                    if(mechanic == null)
+                    {
+                        mechanic = new()
+                        {
+                            Name = mechanicName,
+                            CreatedDate = now,
+                            LastModifiedDate = now
+                        };
+                        _context.Mechanics.Add(mechanic);
+                        existingMechanics.Add(mechanicName, mechanic);
+                    }
+                    _context.BoardGames_Mechanics.Add(new()
+                    {
+                        BoardGame = boardGame,
+                        Mechanic = mechanic,
+                        CreatedDate= now
+                    });
+                }
+        }
+
+        
+        await _context.SaveChangesAsync();
+
+        return new JsonResult(new
+        {
+            BoardGames = _context.BoardGames.Count(),
+            Domains = _context.Domains.Count(),
+            Mechanics = _context.Mechanics.Count(),
+            SkippedRows = skippedRows
+        });
     }
 }
